5ca491b0a924771a8a8170581d7d566c67fc9b4a
Selena Zhou <58157434+zhouselena@users.noreply.github.com>
2023-05-25 11:53:14 -0400

    basic linked to firestore, counts and stores command count

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.members = True
 intents.presences = True
-intents.message_content = True      # important!!
+intents.message_content = True  # important!!
 client = discord.Client(intents=intents)
 
 TOKEN = os.environ.get('TOKEN')
@@ -34,6 +34,47 @@ db = firestore.client()
 
 """FUNCTIONS"""
 
+
+# FIRESTORE: Count commands
+
+def get_cmd_count(user):
+    if not user.bot:
+        doc_ref = db.collection('leaderboard').document(u'{}'.format(user.id))
+        cmd_count_ref = doc_ref.get({u'cmd_count'}).to_dict()
+        if cmd_count_ref is not None:
+            return cmd_count_ref.get('cmd_count')
+        return 0
+
+
+def update_cmd_count(user):
+    if not user.bot:
+        doc_ref = db.collection(u'leaderboard').document(u'{}'.format(user.id))  # what is u?
+        curr_count = get_cmd_count(user)
+        doc_ref.set({
+            u'cmd_count': curr_count + 1
+        })
+
+
+# FIRESTORE: Count coins
+
+def get_coin_count(user):
+    if not user.bot:
+        doc_ref = db.collection('leaderboard').document(u'{}'.format(user.id))
+        cmd_count_ref = doc_ref.get({u'coins'}).to_dict()
+        if cmd_count_ref is not None:
+            return cmd_count_ref.get('coins')
+        return 0
+
+
+def update_coin_count(user, addCoins):
+    if not user.bot:
+        doc_ref = db.collection(u'leaderboard').document(u'{}'.format(user.id))  # what is u?
+        curr_coins = get_coin_count(user)
+        doc_ref.set({
+            u'coins': curr_coins + addCoins
+        })
+
+
 """EVENTS"""
 
 
@@ -44,7 +85,6 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-
     if message.guild is not None:
 
         if message.author == client.user:
@@ -54,9 +94,13 @@ async def on_message(message):
         print(msg)
 
         if msg.startswith("mini"):
+
+            update_cmd_count(message.author)
+
             if msg.startswith("mini hi"):
                 await message.channel.send("Hello!")
-            else:
-                await message.channel.send("I don't recognize that command.")
+            elif msg.startswith("mini cc"):       # count commands
+                command_count = get_cmd_count(message.author)
+                await message.channel.send(f"You have sent me {command_count} commands, {message.author}!")
 
 client.run(TOKEN)
